e56382eca3ecc8c912802422bfd353fd7ddafa3a
ahmed mostafa <engsp00ner51@gmail.com>
2025-02-26 19:22:51 +0200

    fix global.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'js-cookie' {
-    interface CookiesStatic<T = object> {
-      get: (name: string) => string | undefined;
-      set: (name: string, value: string, options?: any) => void;
-      remove: (name: string, options?: any) => void;
-      // ... يمكنك إضافة تعريفات أخرى حسب الحاجة
-    }
-  
-    const Cookies: CookiesStatic;
-    export default Cookies;
+  interface CookiesStatic<T = object> {
+    get: (name: string) => string | undefined;
+    set: (name: string, value: string, options?: any) => void;
+    remove: (name: string, options?: any) => void;
   }
-  
+
+  const Cookies: CookiesStatic;
+  export default Cookies;
+}
