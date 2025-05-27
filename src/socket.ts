2b40b258013042de80a8e19b867392b469d028ea
EngSp00ner <engsp00ner51@gmail.com>
2025-05-27 22:54:33 +0300

    socket fix

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // const socket = io('http://localhost:3000', {
-const socket = io(process.env.BackEnd_Url, {
+const socket = io('https://qualitylead-qms.duckdns.org', {
   transports: ['websocket'],
   withCredentials: true,
 });
